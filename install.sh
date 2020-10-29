106fd81f3ceb27238f655353f9d5bc6ff4823bcf
rayylee <rayylee@foxmail.com>
2020-10-29 22:56:07 +0800

    add oh-my-zsh
    Signed-off-by: rayylee <rayylee@foxmail.com>

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+
+[ -e $HOME/.zshrc ] && rm -rf $HOME/.zshrc
+[ -d $HOME/.oh-my-zsh ] && rm -rf $HOME/.oh-my-zsh
+
+cp -rf ./zsh/oh-my-zsh $HOME/.oh-my-zsh
+cp -f ./zsh/oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
+
+
+echo -e "\033[32m- [Info] You may need to resetting the ENV...\033[0m"
+echo -e "\033[32m- [Info] Install successfully...\033[0m"
+
+exit 0
