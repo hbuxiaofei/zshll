0351b2aa46f15aa430795577727222d133a87470
rayylee <rayylee@foxmail.com>
2020-10-29 23:05:15 +0800

    install: copy zsh-syntax-highlighting and zsh-autosuggestions plugins
    Signed-off-by: rayylee <rayylee@foxmail.com>

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ cd $(dirname $0)
 [ -e $HOME/.zshrc ] && rm -rf $HOME/.zshrc
 [ -d $HOME/.oh-my-zsh ] && rm -rf $HOME/.oh-my-zsh
 
+
 cp -rf ./zsh/oh-my-zsh $HOME/.oh-my-zsh
+cp -rf ./zsh/zsh-syntax-highlighting $HOME/.oh-my-zsh/plugins/
+cp -rf ./zsh/zsh-autosuggestions $HOME/.oh-my-zsh/plugins/
+
 cp -f ./zsh/oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
 
 
