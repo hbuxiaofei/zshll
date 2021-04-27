fa25ce77dd983fb9755c12af4534cb611ce00ee8
rayylee <rayylee@foxmail.com>
2021-04-27 18:41:47 +0800

    install.sh: add /root/.zsh/completions to fpath for compdef

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 
 [ -e $HOME/.zshrc ] && rm -rf $HOME/.zshrc
 [ -d $HOME/.oh-my-zsh ] && rm -rf $HOME/.oh-my-zsh
-
+[ -d $HOME/.zsh ] && rm -rf $HOME/.zsh
 
 cp -rf ./zsh/oh-my-zsh $HOME/.oh-my-zsh
 cp -rf ./zsh/zsh-syntax-highlighting $HOME/.oh-my-zsh/plugins/
@@ -12,6 +12,10 @@ cp -rf ./zsh/zsh-autosuggestions $HOME/.oh-my-zsh/plugins/
 
 cp -f ./zsh/oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
 echo "export TERM=xterm-256color" >> $HOME/.zshrc
+
+mkdir -p $HOME/.zsh/completions
+echo "fpath=($HOME/.zsh/completions \$fpath)" >> $HOME/.zshrc
+echo "autoload -U compinit && compinit" >> $HOME/.zshrc
 
 
 echo -e "\033[32m- [Info] You may need to resetting the ENV...\033[0m"
